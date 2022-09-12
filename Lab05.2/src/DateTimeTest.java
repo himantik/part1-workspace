da8b2d41adc0ae55afe18aaf363ef541d14707f7
himantik <himantik@yahoo.com>
2022-09-12 15:48:19 -0400

    Completed Lab05.2 (dates and times)

--- Lab05.2/src/DateTimeTest.java
+++ Lab05.2/src/DateTimeTest.java
@@ -12,58 +12,79 @@
  * For the purposes of this lab, we give you a few wildcarded imports,
  * so you can use the classes in these packages without having to worry about importing them.
  */
+
 import java.time.*;
 import java.time.format.*;
 
 class DateTimeTest {
 
-    /**
-     * main() calls each of the test methods in turn (currently commented out).
-     * To run one test method at a time, uncomment the call to the one you want to execute.
-     */
-    public static void main(String[] args) {
-        // testNow();
-        // testCreate();
-        // testParse();
-        // testFormat();
-    }
+  /**
+   * main() calls each of the test methods in turn (currently commented out).
+   * To run one test method at a time, uncomment the call to the one you want to execute.
+   */
+  public static void main(String[] args) {
+    testNow();
+    testCreate();
+    testParse();
+    testFormat();
+  }
 
-    /**
-     * TASK: create current date, time, and date-time via now() and then print them.
-     */
-    public static void testNow() {
-        // TODO
-    }
+  /**
+   * TASK: create current date, time, and date-time via now() and then print them.
+   */
+  public static void testNow() {
+    System.out.println(LocalDate.now());
+    LocalDate Yo = LocalDate.now();
+    System.out.println(Yo);
+    LocalDateTime Yot = LocalDateTime.now();
+    System.out.println(Yot);
 
-    /**
-     * TASK: implement the TODOs and print your results.
-     */
-    public static void testCreate() {
-        // TODO: create your birthday via of(). What day of the week were you born on?
 
-        // TODO: use of() to create a value representing the 1st lunar landing - it happened on 7/20/69 at 3:18pm Eastern Time.
-        // NOTE: ignore time-zone, just assume Eastern Time is the local time.
-    }
+  }
 
-    /**
-     * TASK: implement the TODOs and print your results.
-     */
-    public static void testParse() {
-        // TODO: create your birthday by parsing a text representation in standard format ("yyyy-MM-dd").
+  /**
+   * TASK: implement the TODOs and print your results.
+   */
+  public static void testCreate() {
 
-        // OPTIONAL: now create it by parsing text in the form "2/6/2014" (this is Feb 6, not Jun 2).
-    }
+    LocalDate birthday = LocalDate.of(1984, 01, 01);
+    System.out.println(birthday);
 
-    /**
-     * TASK: create formatted display strings for the date below, in the specified formats.
-     */
-    public static void testFormat() {
-        LocalDate hastings = LocalDate.of(1066, 10, 14);
+    LocalDateTime lunarlanding = LocalDateTime.of(1984, 01, 01, 23, 5);
+    System.out.println(lunarlanding);
 
-        // TODO: 10/14/1066
+    // TODO: create your birthday via of(). What day of the week were you born on?
 
-        // TODO: 14-10-1066
+    // TODO: use of() to create a value representing the 1st lunar landing - it happened on 7/20/69 at 3:18pm Eastern Time.
+    // NOTE: ignore time-zone, just assume Eastern Time is the local time.
+  }
 
-        // OPTIONAL: October 14, 1066
-    }
+  /**
+   * TASK: implement the TODOs and print your results.
+   */
+  public static void testParse() {
+    LocalDate birthday = LocalDate.parse("1984-1-01");
+    System.out.println(birthday);
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+    birthday = LocalDate.parse("01/01/1984 , formatter");
+    System.out.println(birthday);
+    // TODO: create your birthday by parsing a text representation in standard format ("yyyy-MM-dd").
+
+    // OPTIONAL: now create it by parsing text in the form "2/6/2014" (this is Feb 6, not Jun 2).
+  }
+
+  /**
+   * TASK: create formatted display strings for the date below, in the specified formats.
+   */
+  public static void testFormat() {
+    LocalDate hastings = LocalDate.of(1066, 10, 14);
+    DateTimeFormatter usDateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    // TODO: 10/14/1066
+
+    // TODO: 14-10-1066
+
+    // OPTIONAL: October 14, 1066
+  }
 }
